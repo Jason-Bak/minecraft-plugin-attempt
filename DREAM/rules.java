8f3772e6e2d451369d0977ca35cd5be35082bdc3
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-12-13 22:24:48 -0500

    Update rules.java

--- DREAM/rules.java
+++ DREAM/rules.java
@@ -4,12 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.junit.Test;
 
 
 //test just to make sure it works
 @EventHandler
-    public void onMobDeath(EntityDeathEvent event) {
+    public void onZombieDeath(EntityDeathEvent event) {
         if(event.getEntity().getKiller() instanceof Player) {
             if(event.getEntity().getType() == EntityType.ZOMBIE){
                 verify(player).sendMessage("Killed a zombie!");
@@ -18,10 +17,13 @@ import org.junit.Test;
     }
 //not a test
 @EventHandler
-    public void onMobDeath(EntityDeathEvent event){
-        if(event.getEntity().getKiller() instanceof Player){
-            if(event.getEntity().getType() == EntityType.BLAZE){
+    public void onBlazeDeath(EntityDeathEvent event){
+        Entity e = event.getEntity();
+        if (e instanceof Blaze) {
+            double chance = random.nextDouble();
+            if (chance <= 0.50) {
                 event.getDrops().add(new ItemStack(Material.BLAZE_ROD, 1));
             }
         }
     }
+//not sure if this is correct
