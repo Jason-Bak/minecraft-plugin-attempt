24bb1f942ed054a53f0d8a2007b1319e5caceb50
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-12-13 21:11:45 -0500

    Update rules.java

--- DREAM/rules.java
+++ DREAM/rules.java
@@ -2,6 +2,17 @@ import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin; 
 
+
+//test
+@EventHandler
+    public void onMobDeath(EntityDeathEvent event) {
+        if(event.getEntity().getKiller() instanceof Player) {
+            if(event.getEntity().getType() == EntityType.ZOMBIE){
+                event.getDrops().add(new ItemStack(Material.DIAMOND, 1));
+            }
+        }
+    }
+
 @EventHandler
     public void onMobDeath(EntityDeathEvent event){
         if(event.getEntity().getKiller() instanceof Player){
